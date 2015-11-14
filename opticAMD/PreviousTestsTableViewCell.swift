2cac554b173255227e3db8d004d07ceee05ba539
Brian Chipman <namedtuple@gmail.com>
2015-11-13 17:08:23 -0800

    Trying alternative design for previous tests screen.

---
+++ opticAMD/PreviousTestsTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PreviousTestsTableViewCell.swift
+//  opticAMD
+//
+//  Created by Brian on 11/13/15.
+//  Copyright © 2015 Med AppJam 2015 - Team 9. All rights reserved.
+//
+
+import UIKit
+
+class PreviousTestsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var resultImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ opticAMD/PreviousTestsViewController.swift
@@ -0,0 +1,91 @@
+//
+//  PreviousTestsViewController.swift
+//  opticAMD
+//
+//  Created by Brian on 11/13/15.
+//  Copyright © 2015 Med AppJam 2015 - Team 9. All rights reserved.
+//
+
+import UIKit
+
+class PreviousTestsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+    @IBOutlet weak var previousTestsTableView: UITableView!
+    // MARK: Properties
+    var savedTestResults = SavedTestResults()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        savedTestResults.load()
+        previousTestsTableView.delegate = self
+        previousTestsTableView.dataSource = self
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Table view data source
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return savedTestResults.count()
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "PreviousTestsTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! PreviousTestsTableViewCell
+        
+        // Fetches the appropriate testResult for the data source layout.
+        let testResult = savedTestResults.get(indexPath.row)
+        
+        // Convert NSDate to String
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        formatter.timeStyle = .ShortStyle
+        let dateString = testResult.date != nil ? formatter.stringFromDate(testResult.date!) : "???"
+        
+        // Assign testResult data to cell and return cell
+        cell.nameLabel.text = dateString
+        cell.resultImageView.image = testResult.leftImage
+        return cell
+    }
+    
+    
+    // Override to support conditional editing of the table view.
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    
+    // Override to support editing the table view.
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == .Delete {
+            
+            // Delete the row from the data source
+            savedTestResults.del(indexPath.row)
+            
+            // Save the testResults array whenever a testResult is deleted.
+            savedTestResults.save()
+            
+            // Delete the row from the view.
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+            
+        else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+        
+    }
+
+}
