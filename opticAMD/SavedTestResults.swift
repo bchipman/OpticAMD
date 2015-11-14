1936a3205c2d7880cba360e6e9ee6fe64116f10a
Brian Chipman <namedtuple@gmail.com>
2015-11-13 19:34:25 -0800

    Renamed app from 'opticAMD' to 'OpticAMD'.

--- opticAMD/SavedTestResults.swift
+++ opticAMD/SavedTestResults.swift
@@ -1,6 +1,6 @@
 //
 //  SavedTestResults.swift
-//  opticAMD
+//  OpticAMD
 //
 //  Created by Brian on 11/8/15.
 //  Copyright © 2015 Med AppJam 2015 - Team 9. All rights reserved.
@@ -10,11 +10,11 @@ import Foundation
 import UIKit
 
 class SavedTestResults {
-    
+
     // MARK: Properties
     private var testResults = [TestResult]()
-    
-    
+
+
     // MARK: Initialization
     init() {
         // Load any saved testResults, otherwise load sample data.
@@ -25,30 +25,30 @@ class SavedTestResults {
             loadSampleTestResults()
         }
     }
-    
+
     private func loadSampleTestResults() {
         let testResult1 = TestResult(date: NSDate(), leftImage: UIImage(named: "cat")!, rightImage: UIImage(named: "cat")!)!
         let testResult2 = TestResult(date: NSDate(), leftImage: UIImage(named: "amslerGrid")!, rightImage: UIImage(named: "amslerGrid")!)!
         testResults += [testResult1, testResult2]
     }
-    
+
     func count() -> Int {
         return testResults.count
     }
-    
+
     func get(row: Int) -> TestResult {
         return testResults[row]
     }
-    
+
     func del(row: Int) {
         testResults.removeAtIndex(row)
     }
-    
+
     func add(testResult: TestResult) {
         testResults.append(testResult)
     }
-    
-    
+
+
     // MARK: NSCoding
     func save() {
         let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(testResults, toFile: TestResult.ArchiveURL.path!)
@@ -58,8 +58,9 @@ class SavedTestResults {
             print("Failed to save testResults...")
         }
     }
-    
+
     func load() -> [TestResult]? {
-        return (NSKeyedUnarchiver.unarchiveObjectWithFile(TestResult.ArchiveURL.path!) as? [TestResult])!.reverse()
+//        return (NSKeyedUnarchiver.unarchiveObjectWithFile(TestResult.ArchiveURL.path!) as? [TestResult])!.reverse()
+        return (NSKeyedUnarchiver.unarchiveObjectWithFile(TestResult.ArchiveURL.path!) as? [TestResult])
     }
 }
