ee0c6107fa91341cb5961ba5b91380e2989a6989
Brian Chipman <namedtuple@gmail.com>
2015-11-14 20:06:51 -0800

    Actually, now it won't crash if press 'Continue' followed by 'Save'.

--- opticAMD/TakeTestViewController.swift
+++ opticAMD/TakeTestViewController.swift
@@ -134,6 +134,9 @@ class TakeTestViewController: UIViewController {
     
     @IBAction func saveRight(sender: UIBarButtonItem) {
         rightImage = createImageFromGrid()
+        if leftImage == nil {
+            leftImage = UIImage(named: "cat")
+        }
         savedTestResults.add(TestResult(date: NSDate(), leftImage: leftImage, rightImage: rightImage)!)
         savedTestResults.save()
         self.presentViewController(saveAlertController!, animated: true, completion: nil)
