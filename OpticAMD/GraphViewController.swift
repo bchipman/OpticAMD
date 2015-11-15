281706da681c3c1a1bfcca223d7e1f076ffe68b6
Brian Chipman <namedtuple@gmail.com>
2015-11-15 10:33:07 -0800

    Graph working with new sample data and two data sets on single graph.

--- OpticAMD/GraphViewController.swift
+++ OpticAMD/GraphViewController.swift
@@ -11,33 +11,88 @@ import Charts
 
 class GraphViewController: UIViewController {
 
+    // MARK: IBOutlets
     @IBOutlet weak var lineChartView: LineChartView!
     
+    // MARK: Properties
+    var savedTestResults = SavedTestResults()
+    
+    // MARK: Setup
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
-        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
-        setChart(months, values: unitsSold)
+        savedTestResults.load()
+//        setExampleGraph()
+        setupMyTestGraph()
+        debug()
     }
     
-    func setChart(dataPoints: [String], values: [Double]) {
+    // MARK: Debug
+    func debug() {
+        
+        let epochHour = 3600.0
+        let epochDay = 86400.0
+        let epochWeek = 604800.0
+        let epochMonth = 2629743.0
+        let epochYear = 31556926.0
+        
+        for i in 0 ... (savedTestResults.count() - 1) {
+            let testResult = savedTestResults.get(i)
+            print("")
+//            print("\n\(i)\ndate: \(testResult.date!)\nleft area: \(testResult.leftImageAreaData)\nright area: \(testResult.rightImageAreaData)")
+            print("\n\(i)\ndate: \(testResult.date!)\nseconds since epoch: \(testResult.date!.timeIntervalSince1970)")
+            
+            let epochTime = testResult.date!.timeIntervalSince1970
+            
+            
+            
+        }
+    }
+    
+    // MARK: Sample Graph
+    func setupMyTestGraph() {
+        let dates = [11.000,11.033,11.067,11.100,11.133,11.167,11.200,11.233,11.267,11.300,11.333,11.367,11.400,11.433,11.467,11.500,11.533,11.567,11.600,11.633,11.667,11.700,11.733,11.767,11.800,11.833,11.867,11.900,11.933,11.967,12.000]
+        let areas = [0.19,0.09,0.15,0.15,0.11,0.15,0.19,0.18,0.09,0.09,0.15,0.08,0.17,0.10,0.20,0.17,0.18,0.19,0.09,0.08,0.19,0.18,0.20,0.10,0.18,0.08,0.17,0.08,0.18,0.11,0.14]
+        let areas2 = [0.41,0.33,0.4,0.43,0.28,0.27,0.47,0.32,0.38,0.32,0.43,0.4,0.48,0.26,0.45,0.26,0.4,0.47,0.31,0.38,0.43,0.5,0.48,0.33,0.37,0.28,0.26,0.36,0.3,0.36,0.25]
 
+        var dataEntries: [ChartDataEntry] = []
+        for i in 0..<dates.count {
+            let dataEntry = ChartDataEntry(value: areas[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        var dataEntries2: [ChartDataEntry] = []
+        for i in 0..<dates.count {
+            let dataEntry = ChartDataEntry(value: areas2[i], xIndex: i)
+            dataEntries2.append(dataEntry)
+        }
+        
+        let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "Blue")
+        let lineChartDataSet2 = LineChartDataSet(yVals: dataEntries2, label: "Orange")
+        
+        let allLineChartData = LineChartData(xVals: dates, dataSets: [lineChartDataSet, lineChartDataSet2])
+
+        lineChartView.data = allLineChartData
+        lineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+    }
+    
+    
+    
+    func setExampleGraph() {
+        let dataPoints = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let values = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
         var dataEntries: [ChartDataEntry] = []
         
         for i in 0..<dataPoints.count {
             let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
             dataEntries.append(dataEntry)
         }
-        
-        
+    
         let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "Units Sold")
         let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
         lineChartView.data = lineChartData
         lineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
         
         lineChartDataSet.circleColors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
-        
     }
 
 }
